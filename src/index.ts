8f527326d33b1f5f110e93491e23885f4a024697
jekdev1999 <jekdev1999@gmail.com>
2024-02-04 20:37:02 -0500

    updated with program id env test file getting keypair

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+
+
+
+
+import * as web3 from "@solana/web3.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
 const world = 'world';
 console.log(`Hello ${world}! `);
+
+
+// KEYPAIR_PRIVATE_KEY should be in the format of a [] with lots of numbers that were generated from your keypair using solana cli
+const secret = JSON.parse(process.env.KEYPAIR_PRIVATE_KEY ?? "") as number[];
+const secretKey = Uint8Array.from(secret)
+const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey)
+
+const programID = new web3.PublicKey(process.env.PROGRAM_ID ?? "");
+
 
